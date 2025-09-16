eea51ccf7154b25c7ee1e5ab1ae86c2071e3dec2
Anthony Cervi <your-anthonycervi@gmail.com>
2025-09-15 20:57:36 -0700

    changed tab name

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+      <>
+        <title>Anthony Cervi | UX/UI Portfolio</title>
+        <meta name="description" content="Anthony Cervi – UX/UI Designer & Front-End Developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    );
+  }
