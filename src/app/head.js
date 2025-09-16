564e0e4cb5c918cc724ca42f652a8580eef4268b
Anthony Cervi <your-anthonycervi@gmail.com>
2025-09-15 21:03:53 -0700

    Update tab title, remove favicon

--- src/app/head.js
+++ src/app/head.js
@@ -1,10 +1,8 @@
-// src/app/head.js
 export default function Head() {
     return (
       <>
         <title>Anthony Cervi | UX/UI Portfolio</title>
         <meta name="description" content="Anthony Cervi – UX/UI Designer & Front-End Developer" />
-        <link rel="icon" href="/favicon.ico" />
       </>
     );
   }
